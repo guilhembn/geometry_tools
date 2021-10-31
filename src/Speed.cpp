215a9aafa3af691ba0d3faf15e35468de6a13270
guilhembn <gbuisan@laas.fr>
2021-10-31 17:11:33 +0100

    First commit geometry_tools

---
+++ src/Speed.cpp
@@ -0,0 +1,7 @@
+#include "GeometryTools/Speed.h"
+
+namespace rd {
+Speed::Speed(double vx, double vy, double vtheta) : linearSpeed_(vx, vy), rotationalSpeed_(vtheta) {}
+
+double Speed::linearSpeed() const { return linearSpeed_.norm(); }
+}  // namespace rd
